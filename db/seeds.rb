3382ab5111e43336e0788b145956e4ac50be62bc
Corentin Dubois <corentindubois22@gmail.com>
2024-08-28 15:28:37 -0600

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Flat.create!(
+  name: 'Flat 1',
+  address: "somewhere over the rainbow",
+  description: "Really noice",
+  price_per_night: 100,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Flat Dos',
+  address: "over there mate",
+  description: "Shady piece of shit",
+  price_per_night: 50,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'El tercero',
+  address: "the other way",
+  description: "Eh.",
+  price_per_night: 70,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Final boss of flats',
+  address: "on top of the hill overlooking everything else like it belongs to it",
+  description: "well ain't that the top of the pops",
+  price_per_night: 2000,
+  number_of_guests: 10
+)
